fa0d5c8d3c90dd25d21fb3c70c7e3a9f26832979
Olia <good-friday@mail.ru>
2021-01-20 20:28:36 +0300

    Автоматическая сборка проекта

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,11 @@
 let pageHeader = document.querySelector('.page-header');
 let mainNavToggle = document.querySelector('.main-nav__toggle');
+let ratesButton = document.querySelector('.profile-promo__business-rates');
+let ratesPopup = document.querySelector('.modal');
+let ratesClose = document.querySelector('.modal__close');
+let filter = document.querySelector('.filter');
+let filterToggle = document.querySelector('.filter__toggle');
+let filterHide = document.querySelector('.filter__hide');
 
 window.onscroll = function () {
   if (window.pageYOffset > 0) {
@@ -20,3 +26,43 @@ mainNavToggle.addEventListener('click', function() {
     pageHeader.classList.remove('page-header--opened');
   }
 });
+
+if (ratesButton) {
+  ratesButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    ratesPopup.classList.add('modal--show');
+  });
+
+  ratesClose.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    ratesPopup.classList.remove('modal--show');
+  });
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      if (ratesPopup.classList.contains('modal--show')) {
+        evt.preventDefault();
+        ratesPopup.classList.remove('modal--show');
+      }
+    }
+  });
+}
+
+if (filter) {
+  filter.classList.remove('filter--nojs');
+
+  filterToggle.addEventListener('click', function() {
+    if (filter.classList.contains('filter--closed')) {
+      filter.classList.remove('filter--closed');
+      filter.classList.add('filter--opened');
+    } else {
+      filter.classList.add('filter--closed');
+      filter.classList.remove('filter--opened');
+    }
+  });
+
+  filterHide.addEventListener('click', function() {
+    filter.classList.remove('filter--opened');
+    filter.classList.add('filter--closed');
+  });
+}
